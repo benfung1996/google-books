28be3ada04e0440fc8e6c97e65fb26285cbfe62a
Ben Fung <benfung1996@gmail.com>
2020-06-10 10:42:16 -0700

    added view.js

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -3,20 +3,24 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NoMatch from "./pages/NoMatch";
 import Saved from "./pages/Saved";
 import Search from "./pages/Search";
+import View from "./pages/View";
+import Nav from "./components/Nav";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Search} />
+          <Route exact path="/books" component={Saved} />
+          <Route exact path="/books/:id" component={View} />
+          <Route component={NoMatch} />
+        </Switch>
       </div>
+    </Router>
     );
   }
 }
