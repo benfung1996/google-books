ee6203ed11fa1ee4117a0c8f6ccea3c642efaa10
Ben Fung <benfung1996@gmail.com>
2020-06-15 10:53:57 -0700

    added build

---
+++ client/build/precache-manifest.afe33d1d786b70be900951a6784e503c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "28184f8f3692ccc2d6c5",
+    "url": "/static/css/main.447986c6.chunk.css"
+  },
+  {
+    "revision": "28184f8f3692ccc2d6c5",
+    "url": "/static/js/main.306b8a7a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a25c5273c94e5d601a03",
+    "url": "/static/js/2.7cef51ad.chunk.js"
+  },
+  {
+    "revision": "b234bb172618509807937df23334d0a1",
+    "url": "/static/media/book.b234bb17.jpg"
+  },
+  {
+    "revision": "8e628abe3fb685ee6be8308ee60f75b5",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes)
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password1@ds115595.mlab.com:15595/heroku_frwjzqp8", { useNewUrlParser: true });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
