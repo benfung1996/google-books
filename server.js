c8499d274424c2b2d9a049731db146bafbb6e98c
Ben Fung <benfung1996@gmail.com>
2020-07-26 12:42:34 -0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets (usually on heroku)
+// Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
